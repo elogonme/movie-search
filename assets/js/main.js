a7749a8af869764442a4c113df8ee46ffc9993de
Eldar <elogon@gmail.com>
2020-11-10 09:17:01 -0500

    Implement fully working app with design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-
-
+    $('#movie-card').hide(); // Hide movie card info
     
+    // Event listener for search button click
     $('#search').on('click', function(e){
         e.preventDefault();
         var title = $('#movie-name').val();
-        getMovies(title);
+        getMovie(title); // Callback Movie search function
     });
 
-    function getMovies(title){
-
+    // Function to search for movie - call API
+    function getMovie(title){
         var endpoint = `http://www.omdbapi.com/?t=${title}&apikey=fac8a5da`;
         fetch(endpoint).then(function(response){
+            $('#error').text('');
             if (response.ok) {
                 return response.json();
             }
@@ -19,38 +20,35 @@ $(document).ready(function(){
         }).then(function(data){
             if (data.Response === 'False') {
                 console.log(data.Error);
+                $('#error').text(data.Error); // Display error message on page
                 return
             }
             
-            displayMovieInfo(data);
+            displayMovieInfo(data); // Call function to display movie info
+            $('#movie-card').show();
             console.log(data);
         }).catch(function(err){
             console.log(err);
+            
         });
 
     }
 
+    // Function to display movie info - reads data object properties and outputs in table on page
     function displayMovieInfo(data){
         var output = '';
         for (var property in data) {
         output += property + ': ' + data[property]+'; ';
         }
-        /*
-        <tbody id="movie-info">
-            <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            </tr>
-        </tbody>
-        */
-        
         $('#poster').attr('src', data.Poster);
         $('#title').text(data.Title)
         $('#actors').text(data.Actors);
+        $('#movie-info').empty();
+        $('#description').text(data.Plot);
         for (var property in data){
             var $tr = $('<tr>');
             var $th = $('<th>');
-            var $td = $('<td class="text-wrap">');
+            var $td = $('<td>');
             $th.attr('scope', 'row');
             $th.text(property + ': ');
             $td.text(data[property])
@@ -58,8 +56,7 @@ $(document).ready(function(){
             $tr.append($td);
             $('#movie-info').append($tr);
         };
-
-        console.log(output);
     };
-})
+    
+});
 
